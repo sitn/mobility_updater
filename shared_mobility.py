93cbc31484dd29aec5b90a4823f24f57c7715a2b
Michael Kalbermatten <michael.kalbermatten@ne.ch>
2024-05-14 09:05:39 +0200

    Update script for shared mobility stations

--- shared_mobility.py
+++ shared_mobility.py
@@ -16,6 +16,63 @@ logging.basicConfig(
     filemode='w'
 )
 
+def get_providers_stations(base_station_url, certificate, payload, station_info_url, bbox, tablename, servers):
+
+    r = requests.get(base_station_url, verify=certificate, params=payload)
+
+    if r.status_code != 200:
+        sys.exit()
+
+    logging.info('Status code of shared mobility, get systems: %s' % r.status_code)
+
+    system_list = r.json()['systems']
+    # Update station list
+    station_ids = []
+    provider_ids = []
+    station_sql_list = []
+
+    for system in system_list:
+
+        r = requests.get(base_station_url+'/'+system['id']+'/'+station_info_url, verify=certificate, params=payload)
+        stations = r.json()['data']['stations']
+
+        station_sql = """
+        INSERT INTO %s (idobj, "name", provider_id, geom, store_uri_android, store_uri_ios) VALUES ('%s', '%s', '%s', %s, '%s', '%s')
+        ON CONFLICT (idobj) DO UPDATE SET
+        "name" = EXCLUDED."name",
+        store_uri_android = EXCLUDED.store_uri_android,
+        store_uri_ios = EXCLUDED.store_uri_ios
+        """
+
+        for station in stations:
+
+            if station['lon'] >= bbox['xmin'] and station['lon'] <= bbox['xmax'] \
+                    and station['lat'] >= bbox['ymin'] and station['lat'] <= bbox['ymax']:
+
+                station_ids.append(station['station_id'])
+                station_sql_list.append(station_sql % (
+                    tablename,
+                    station['station_id'],
+                    station['name'].replace("'", "''"),
+                    system['id'],
+                    "ST_Transform(ST_GeomFromText('POINT(" + str(station['lon']) + " " + str(station['lat']) + ")', 4326), 2056)",
+                    station['rental_uris']['android'] if 'rental_uris' in station else None,
+                    station['rental_uris']['ios'] if 'rental_uris' in station else None,
+                ))
+                if system['id'] not in provider_ids:
+                    provider_ids.append(system['id'])
+
+    run_sql(servers, station_sql_list)
+    logging.info('Updated station list for each provider having stations inside the bbox')
+
+    # Delete all station which are not listed in station_ids
+    delete_sql = ["DELETE FROM %s WHERE idobj not in ('%s')" % (
+        tablename,
+        "','".join(station_ids)
+    )]
+    run_sql(servers, delete_sql )
+    logging.info('Deleted all stations which are not anymore referenced')
+
 
 def get_data(args):
 
@@ -25,105 +82,37 @@ def get_data(args):
     if args.certificate_verification == 'off':
         certificate = False
 
+    station_only = False
+    if args.station_only == 'yes':
+        station_only = True
+
     with open(config_file) as file:
         params = load(file, Loader=FullLoader)
 
+    # User parameters
     bbox = params['bbox']
-    providers_info_url = params['providers_info_url']
+    base_station_url = params['base_station_url']
     station_info_url = params['station_info_url']
     station_status_url = params['station_status_url']
+    payload = {'authorization': params['authorization_email']}
     tablename = params['shared_station_tablename']
-
     servers = params['servers']
 
-    # Get Stations
-    r = requests.get(station_info_url, verify=certificate)
+    if station_only is False:
+        get_providers_stations(base_station_url, certificate, payload, station_info_url, bbox, tablename, servers)
 
-    if r.status_code != 200:
-        sys.exit()
+    updated_stations = []
 
-    logging.info('Status code of shared mobility, get stations: %s' % r.status_code)
+    # Get all providers
+    sql = "SELECT provider_id FROM %s GROUP BY provider_id" % tablename
+    rows = get_once(servers[0], sql)
 
-    stations = r.json()['data']['stations']
-
-    station_ids = []
-
-    station_sql = """
-    INSERT INTO %s (idobj, "name", provider_id, geom) VALUES ('%s', '%s', '%s', %s) ON CONFLICT DO NOTHING
-    """
-
-    station_sql_list = []
-
+    # Get all stations
+    sql = "SELECT idobj FROM %s" % tablename
+    stations = get_once(servers[0], sql)
+    stations_ids = []
     for station in stations:
-        if station['lon'] >= bbox['xmin'] and station['lon'] <= bbox['xmax'] \
-                and station['lat'] >= bbox['ymin'] and station['lat'] <= bbox['ymax']:
-            station_ids.append(station['station_id'])
-            station_sql_list.append(station_sql % (
-                tablename,
-                station['station_id'],
-                station['name'].replace("'", "''"),
-                station['provider_id'],
-                "ST_Transform(ST_GeomFromText('POINT(" + str(station['lon']) + " " + str(station['lat']) + ")', 4326), 2056)"
-            ))
-
-    run_sql(servers, station_sql_list)
-
-    # Check uris (we use the first server to do so, as they all should contain the same data)
-    url_sql = """
-    SELECT provider_id FROM %s WHERE provider_url is null
-    """
-    records = get_once(servers[0], (url_sql % tablename))
-
-    update_urls_sql = """
-    UPDATE %s SET
-        provider_url = '%s',
-        store_uri_android = '%s',
-        store_uri_ios = '%s'
-    WHERE
-        provider_id = '%s'
-    """
-
-    if len(records) > 0:
-        r = requests.get(providers_info_url, verify=certificate)
-
-        logging.info('Status code of shared mobility, get provider info: %s' % r.status_code)
-
-        if r.status_code != 200:
-            sys.exit()
-
-        providers = r.json()['data']['providers']
-        done = []
-
-        provider_sql_list = []
-
-        for record in records:
-
-            if record[0] not in done:
-                provider = list(filter(lambda x: x["provider_id"] == record[0], providers))
-                if len(provider) == 0:
-                    continue
-                provider = provider[0]
-                provider_sql_list.append(update_urls_sql % (
-                    tablename,
-                    provider['url'] if 'url' in provider else '-9999',
-                    provider['rental_apps']['android']['store_uri'] if 'rental_apps' in provider else '-9999',
-                    provider['rental_apps']['ios']['store_uri'] if 'rental_apps' in provider else '-9999',
-                    record[0],
-                ))
-
-                done.append(record[0])
-
-        run_sql(servers, provider_sql_list)
-
-    # Check vehicle availability
-    r = requests.get(station_status_url, verify=certificate)
-
-    logging.info('Status code of shared mobility, get provider availability: %s' % r.status_code)
-
-    if r.status_code != 200:
-        sys.exit()
-
-    stations = r.json()['data']['stations']
+        stations_ids.append(station[0])
 
     station_sql = """
     UPDATE %s SET
@@ -132,33 +121,40 @@ def get_data(args):
         is_returning = %s,
         last_reported = '%s',
         num_bikes_available = %s,
+        num_docks_available = %s,
         update_time = '%s'
     WHERE
         idobj = '%s'
     """
 
     now = datetime.now().isoformat()
-
     station_sql_list = []
 
-    for station_id in station_ids:
-        station = list(filter(lambda x: x["station_id"] == station_id, stations))
+    for row in rows:
 
-        if len(station) == 0:
-            continue
+        r = requests.get(base_station_url+"/"+row[0]+"/"+station_status_url, verify=certificate, params=payload)
 
-        station = station[0]
+        logging.info('Status code of shared mobility, get provider info: %s' % r.status_code)
 
-        station_sql_list.append(station_sql % (
-            tablename,
-            str(station['is_installed']),
-            str(station['is_renting']),
-            str(station['is_returning']),
-            datetime.fromtimestamp(station['last_reported']).isoformat(),
-            str(station['num_bikes_available']),
-            now,
-            station['station_id'],
-        ))
+        if r.status_code != 200:
+            sys.exit()
+
+        stations = r.json()['data']['stations']
+        for station in stations:
+            if station['station_id'] in stations_ids:
+                stations_ids.remove(station['station_id'])
+
+                station_sql_list.append(station_sql % (
+                    tablename,
+                    str(station['is_installed']),
+                    str(station['is_renting']),
+                    str(station['is_returning']),
+                    datetime.fromtimestamp(station['last_reported']).isoformat(),
+                    str(station['num_bikes_available']),
+                    str(station['num_docks_available']) if 'num_docks_available' in station else 'null',
+                    now,
+                    station['station_id'],
+                ))
 
     run_sql(servers, station_sql_list)
 
@@ -179,6 +175,14 @@ if __name__ == '__main__':
         default='on',
         action='store'
     )
+    parser.add_argument(
+        '-s',
+        '--station_only',
+        help='If set to "yes", then the script only updates the station information and does not check for new station or providers (default is "no")',
+        default='no',
+        action='store'
+    )
+
     args = parser.parse_args()
 
     get_data(args)
